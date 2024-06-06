c07793ce9da71bb6d75e8e399884d27754a2865e
hzw <huangzw0410@163.com>
2024-06-06 23:39:16 +0800

    create ChatBotApp and add layout

---
+++ ChatBotApp/app/src/main/java/com/hwl/chatbotapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hwl.chatbotapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
